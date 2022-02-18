21ad09abf974fc5d8b6987668f5aaefe540087da
Petri Riihikallio <petri.riihikallio@metis.fi>
2022-02-18 22:10:25 +0200

    2d

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -1,34 +1,42 @@
+#include <stdlib.h>
 #include	"unp.h"
 
 int
 main(int argc, char **argv)
 {
-	int					sockfd, n;
+	int					sockfd, n, num;
 	char				recvline[MAXLINE + 1];
 	struct sockaddr_in	servaddr;
 
-	if (argc != 2)
-		err_quit("usage: a.out <IPaddress>");
-
-	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		err_sys("socket error");
+	if (argc != 3)
+		err_quit("usage: a.out <IPaddress> <numConns>");
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port   = htons(50013);	/* daytime server */
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 		err_quit("inet_pton error for %s", argv[1]);
+	
+	num = strtol(argv[2]);
+	if (!num) num = 1;
+	for(int i = 0; i < num; i++) {
+		if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+			err_sys("socket error");
 
-	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
-		err_sys("connect error");
+		if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
+			err_sys("connect error");
 
-	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
-		recvline[n] = 0;	/* null terminate */
-		if (fputs(recvline, stdout) == EOF)
-			err_sys("fputs error");
+		while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+			recvline[n] = 0;	/* null terminate */
+			if (fputs(recvline, stdout) == EOF)
+				err_sys("fputs error");
+		}
+		if (n < 0)
+			err_sys("read error");
+
+		if (shutdown(sockfd, 2) < 0)
+			err_sys("socket close error");
 	}
-	if (n < 0)
-		err_sys("read error");
 
 	exit(0);
 }
